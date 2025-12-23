589e11643954f45ada902105dae108430d967356
Christopher Enam Franklin <cef1911@hotmail.com>
2025-12-22 19:49:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ from PIL import Image
 #import plotly.figure_factory as ff
 
 
-pdf_url = ("file:///C:/Users/cef19/Downloads/Caleb%20Official%20resume%20(1).pdf")
+#pdf_url = ("file:///C:/Users/cef19/Downloads/Caleb%20Official%20resume%20(1).pdf")
 
 st.pdf(pdf_url, height=600)
 
