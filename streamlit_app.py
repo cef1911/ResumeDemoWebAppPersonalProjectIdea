9c0ce91d8d74fd63698d89691a0bccf69721fcb6
Christopher Enam Franklin, MS <cef1911@hotmail.com>
2026-03-01 14:34:36 -0500

    Update resume template link in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,8 @@ See the website for more info https://streamlit.io/
 
 Click the link to view a sample resume template web app
 
-https://acrobat.adobe.com/id/urn:aaid:sc:us:39641afc-f344-464a-8ebf-f5eaea866ace
+https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:d33aff50-700a-4c3f-9a55-2e919957b1a9
+
 
 """
 
